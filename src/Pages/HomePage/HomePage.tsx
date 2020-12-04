3e6494074f3cccb77d4244a64049b3060ecdcd34
Julien Lavocat <julien.lavocat@gmail.com>
2020-12-04 01:34:40 +0100

    Improved homepage

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,12 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
-import { Container, Col, Row, Form, Card, Navbar } from "react-bootstrap";
+import {
+	ButtonGroup,
+	Container,
+	Row,
+	Form,
+	Navbar,
+	Col,
+	Button,
+	FormGroup,
+	InputGroup,
+	CardGroup,
+	Card,
+	Table,
+} from "react-bootstrap";
+import {
+	faFacebook,
+	faTwitter,
+	faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 export class HomePage extends Component {
 	render() {
 		return (
 			<Container fluid className="min-100">
-				<Navbar bg="primary">
-					<Navbar.Brand href="#home" className="justify-content-right">
+				<Navbar className="bg-info justify-content-between nav-fill" fixed="top">
+					<Row>
+						<FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+						<FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+						<FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+					</Row>
+					<Navbar.Brand
+						href="#home"
+						className="justify-content-right"
+					>
 						<img
 							alt=""
 							src="/logo-64.jpg"
@@ -16,50 +43,68 @@ export class HomePage extends Component {
 						/>
 						Clean Waves
 					</Navbar.Brand>
-				</Navbar>
 
-				{/* <Row>
-					<Col sm={8}>
-						<Row className="d-flex justify-content-center">
-							<Form inline>
-								<button
-									className="btn btn-outline- ml-2"
-									type="submit"
-								>
-									Search
-								</button>
-								<input
-									type="text"
-									placeholder="Search.."
-									id="mainSearchBar"
-								></input>
-							</Form>
-						</Row>
-						<Row className="align-self-center">
-							<Card>
+					<ButtonGroup>
+						<Button>Connexion</Button>
+						<Button>Inscription</Button>
+					</ButtonGroup>
+				</Navbar>
+				<Row className="justify-content-center mb-4 mt-1">
+					<Form inline>
+						<InputGroup>
+							<Button variant="outline-dark" type="submit">
+								Recherche
+							</Button>
+							<input
+								type="text"
+								placeholder="Spots, évènements, ..."
+								id="mainSearchBar"
+							></input>
+						</InputGroup>
+					</Form>
+				</Row>
+				<Row>
+					<Col sm={9} className="mt-5">
+						<CardGroup>
+							<Card onClick={(e: any) => console.log("Click on clard")}>
 								<Card.Body>
-									<Card.Title>Faire un signalement</Card.Title>
+									<Card.Title>Faire un rapport</Card.Title>
 								</Card.Body>
 							</Card>
-							<Card>
+							<Card onClick={(e: any) => console.log("Click on clard")}>
 								<Card.Body>
 									<Card.Title>Spots favoris</Card.Title>
 								</Card.Body>
 							</Card>
-							<Card>
+							<Card onClick={(e: any) => console.log("Click on clard")}>
 								<Card.Body>
-									<Card.Title>Prévisions météos</Card.Title>
+									<Card.Title>Liste des spots</Card.Title>
 								</Card.Body>
 							</Card>
-						</Row>
+							<Card onClick={(e: any) => console.log("Click on clard")}>
+								<Card.Body>
+									<Card.Title>Profil</Card.Title>
+								</Card.Body>
+							</Card>
+						</CardGroup>
 					</Col>
-					<Col sm={4}>
-						<Row>
-							<img src="/logo-64.jpg"></img>
-							<p>Clean Waves</p>
-						</Row>
+					<Col sm={3}>
+						<Table bordered striped>
+							<thead>
+								<tr>
+									<th>Derniers signalements</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>Spot de l'enfer</tr>
+								<tr>Spot de je ne sais ou</tr>
+								<tr>Spot pas la</tr>
+								<tr>Spot caché</tr>
+								<tr>Spot pas trouvé</tr>
+							</tbody>
+						</Table>
 					</Col>
-				</Row> */}
+				</Row>
 			</Container>
 		);
 	}
